95b4ff53a966891767aca7df226d4f5917ada8e5
GlebEvemi <gleb2466@gmail.com>
2025-06-26 05:14:54 +0300

    DVD-28 <Add form login>

---
+++ controller/Controller.php
@@ -0,0 +1,123 @@
+<?php
+
+class Controller
+{
+
+    public static function StartSite()
+    {
+        $lang = $_GET['lang'] ?? 'ee';
+        $arr = Disk::getAllDisks($lang);
+        include_once 'view/start.php';
+    }
+
+    public static function AllGenres()
+    {
+        $lang = $_GET['lang'] ?? 'ee';
+        $arr = Genres::getAllGenres($lang);
+        include_once 'view/genres.php';
+    }
+
+    public static function AllDisks()
+    {
+        $lang = $_GET['lang'] ?? 'ee';
+        $arr = Disk::getAllDisks($lang);
+        include_once 'view/disk.php';
+        ob_start();
+        ViewDisks::AllDisks($arr);
+        $content = ob_get_clean();
+        include_once 'view/layout.php';
+    }
+
+    public static function DiskByGenre($genre_id)
+    {
+        $lang = $_GET['lang'] ?? 'ee';
+        $arr = Disk::getDisksByGenreID($lang, $genre_id);
+        include_once 'view/disk.php';
+        ob_start();
+        ViewDisks::AllDisks($arr);
+        $content = ob_get_clean();
+        include_once 'view/layout.php';
+    }
+
+    public static function DiskByID($id)
+    {
+        $n = Disk::getDiskByID($id);
+        include_once 'view/readnews.php';
+    }
+
+    public static function error404()
+    {
+        include_once 'view/error404.php';
+    }
+
+
+    public static function InsertComment($comment, $diskId)
+    {
+
+        $diskId = (int)$diskId;
+        $comment = trim($comment);
+
+        if (empty($comment)) {
+            die("Текст комментария не может быть пустым");
+        }
+
+        if ($diskId <= 0) {
+            die("Неверный ID новости");
+        }
+
+        $db = new Database();
+
+        $disk = $db->getOne("SELECT id FROM disks WHERE id = ?", [$diskId]);
+        if (!$disk) {
+            die("Диск с ID $diskId не найдена");
+        }
+
+        $result = Comments::insertComment($comment, $diskId);
+
+        if ($result) {
+            header("Location: disk?id=" . $diskId);
+            exit();
+        } else {
+            die("Ошибка при добавлении комментария");
+        }
+    }
+
+
+    public static function Comments($diskid)
+    {
+        $arr = Comments::getCommentByDiskID($diskid);
+        ViewComments::CommentsByDisk($arr);
+    }
+
+    public static function CommentsCount($diskid)
+    {
+        $arr = Comments::getCommentsCountByDiskID($diskid);
+        ViewComments::CommentsCount($arr);
+    }
+
+    public static function CommentsCountWithAncor($diskid)
+    {
+        $arr = Comments::getCommentsCountByDiskID($diskid);
+        ViewComments::CommentsCountWithAncor($arr);
+    }
+
+    public static function registerForm()
+    {
+        include_once('view/formRegister.php');
+    }
+    public static function loginForm()
+    {
+        include_once('view/formLogin.php');
+    }
+    public static function registerUser()
+    {
+        $result = Register::registerUser();
+        include_once('./index.php');
+    }
+
+    public static function loginUser()
+    {
+        $result = Register::login();
+        include_once('./index.php');
+    }
+}
---
+++ view/formLogin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Register User</title>
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="login.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel-default">
+                    <div class="panel-heading">
+                        <h3>Login</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="loginAnswer">
+                            <div class="form-group">
+                                <label for="name" class="col-md-4 control-label">Name</label>
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control" name="name" value="" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn-primary" name="save">
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
+                            <p style="padding-top:10px;"><a href="./">Web site</a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
